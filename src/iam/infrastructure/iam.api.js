a332b39b12d27fa611658abc29c9ef48fd59ae96
MaxghZZ <milenkorvu@gmail.com>
2026-07-03 01:26:57 -0500

    fix(iam): connect login and register to real backend authentication endpoints

--- src/iam/infrastructure/iam.api.js
+++ src/iam/infrastructure/iam.api.js
@@ -2,15 +2,27 @@ import { BaseApi } from '../../shared/infrastructure/base-api.js';
 import { BaseEndpoint } from '../../shared/infrastructure/base-endpoint.js';
 import { UserAssembler } from './assemblers/user.assembler.js';
 
-const iamApiUrl = import.meta.env.VITE_IAM_API_URL;
+const authEndpointPath = import.meta.env.VITE_AUTH_ENDPOINT_PATH ?? '/authentication';
 const usersEndpointPath = import.meta.env.VITE_IAM_USERS_ENDPOINT_PATH ?? '/users';
 
 export class IamApi extends BaseApi {
+  #authEndpointPath;
   #usersEndpoint;
 
   constructor() {
-    super(iamApiUrl);
+    super();
+    this.#authEndpointPath = authEndpointPath;
     this.#usersEndpoint = new BaseEndpoint(this, usersEndpointPath);
+  }
+
+  async signIn(email, password) {
+    const response = await this.http.post(`${this.#authEndpointPath}/sign-in`, { email, password });
+    return response.data;
+  }
+
+  async signUp(email, password) {
+    const response = await this.http.post(`${this.#authEndpointPath}/sign-up`, { email, password });
+    return response.data;
   }
 
   async getUsers() {
